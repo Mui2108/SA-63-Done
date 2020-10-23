146573dc6226db4707c3c552a8350f8a07ac3fc3
Mui2108 <56442738+Mui2108@users.noreply.github.com>
2020-10-23 18:00:43 +0700

    Frontend

---
+++ Frontend/plugins/welcome/src/components/TablePatient/TPatient.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { withStyles, Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+
+const StyledTableRow = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }),
+)(TableRow);
+
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+  return { name, calories, fat, carbs, protein };
+}
+
+
+export default function CustomizedTables() {
+
+  
+  return (
+    <div>
+      
+    </div>
+  );
+}
